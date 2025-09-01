81bf2a79533ff374a7e3055768a8c0138ae0d6eb
Imam Samir Ali <samirali.tech88@gmail.com>
2025-09-01 21:54:42 +0600

    Create CF_1748A.cpp

---
+++ CF_1748A.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;//test case
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ans=(n+1)/2;
+        cout<<ans<<endl;
+    }
+return 0;
+}
